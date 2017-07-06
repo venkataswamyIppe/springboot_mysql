a534ca4c9c1c18119732ae4ff1f4ca675cc83212
venkataswamyIppe <ippe.venkataswamy@gmail.com>
2017-07-06 13:46:04 +0530

    welcome commit

---
+++ welcome/src/main/java/com/hello/WelcomeApplication.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApplication.class, args);
+	}
+}
